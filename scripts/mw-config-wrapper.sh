1f907e714dc0c7cf38d1dc076a8da1a8da6335e1
gregd72002 <gregd72002@gmail.com>
2016-04-10 23:28:08 +0100

    Adding logging

--- scripts/mw-config-wrapper.sh
+++ scripts/mw-config-wrapper.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-while [ 1 ]; do
-	CMD=`sed -n '2p' $1`
-	ARGS=`sed -n '3p' $1`
-	sleep 1;
-	$CMD $ARGS
-done
+CMD=`sed -n '2p' $1`
+ARGS=`sed -n '3p' $1`
+LOG=/tmp/${CMD}.log
 
+while [ 1 ]; do
+        sleep 1;
+        echo "Starting $CMD $ARGS" >> /tmp/mw-config.log
+        $CMD $ARGS > ${LOG} 2>&1
+        echo "$CMD has stopped." >> /tmp/mw-config.log
+done
