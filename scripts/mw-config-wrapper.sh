b2f82c7317a437e502f7315f181794ad7300169f
Gregory Dymarek <gregd72002@gmail.com>
2016-04-23 15:05:59 +0000

    Wrapper not refreshing fixed

--- scripts/mw-config-wrapper.sh
+++ scripts/mw-config-wrapper.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-CMD=`sed -n '2p' $1`
-ARGS=`sed -n '3p' $1`
-LOG=/tmp/${CMD}.log
-
 while [ 1 ]; do
         sleep 1;
+
+        CMD=`sed -n '2p' $1` #this needs to be evaluated every run otherwise it's not possible to hot change parameters without reboot
+        ARGS=`sed -n '3p' $1`
+        LOG=/tmp/${CMD}.log
+
         echo "Starting $CMD $ARGS" >> /tmp/mw-config.log
         $CMD $ARGS > ${LOG} 2>&1
         echo "$CMD has stopped." >> /tmp/mw-config.log
